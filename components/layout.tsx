50f1b621987c7ac35929370fdcd4dda0a37ce770
lmk0420 <lmk19960420@gmail.com>
2020-11-22 15:58:37 +0000

    remove tutorial items

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,13 +13,7 @@ export const Layout: FC<{ home: boolean }> = ({ children, home }) => {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Learn how to build a personal website using Next.js" />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            SITE_TITLE
-          )}.png?heme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        ></meta>
+        <meta name="description" content="mako_makokのブログ" />
         <meta name="og:title" content={SITE_TITLE} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
