6722331a5d6fba7b438af01adfcb967188a9b23a
lmk0420 <lmk19960420@gmail.com>
2020-11-22 08:11:41 +0000

    separation post data list

---
+++ components/PostItemList.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react'
+import Link from 'next/link'
+import Date from '../components/date'
+import { PostData } from '../lib/post'
+import utilStyles from '../styles/utils.module.css'
+
+interface Props {
+  postDatas: PostData[]
+}
+export const PostItemList: FC<Props> = (props) => {
+  const { postDatas } = props
+  return (
+    <ul className={utilStyles.list}>
+      {postDatas.map(({ id, date, title }) => (
+        <li className={utilStyles.listItem} key={id}>
+          <Link href={`/posts/${id}`}>
+            <a>{title}</a>
+          </Link>
+          <br />
+          <small className={utilStyles.lightText}>
+            <Date dateString={date} />
+          </small>
+        </li>
+      ))}
+    </ul>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
-import Date from '../components/date'
+import { PostItemList } from '../components/PostItemList'
 import Layout, { SITE_TITLE } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostData, PostData } from '../lib/post'
@@ -23,19 +22,7 @@ const Home: FC<Props> = (props) => {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <PostItemList postDatas={allPostData} />
       </section>
     </Layout>
   )
