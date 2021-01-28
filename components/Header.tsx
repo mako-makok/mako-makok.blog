f4dbd69b52724552e2d309250a359f7bb973bb4f
mako-makok <lmk19960420@gmail.com>
2021-01-28 22:54:25 +0900

    using Link component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,13 +5,13 @@ export const Header: FC = () => {
   return (
     <header className="flex items-center justify-between p-2 border-b bg-gray-800">
       <Link href="/">
-        <a className="text-gray-100  font-bold text-2xl no-underline">{'mako_makok.dev'}</a>
+        <a className="text-gray-100  font-bold text-2xl no-underline">mako_makok.dev</a>
       </Link>
       <ul className="inline-flex items-center">
         <li className="px-2 md:px-4">
-          <a href="/" className="text-gray-300 font-semibold hover:text-gray-200">
-            {'Home'}
-          </a>
+          <Link href="/">
+            <a className="text-gray-300 font-semibold hover:text-gray-200">Home</a>
+          </Link>
         </li>
       </ul>
     </header>
